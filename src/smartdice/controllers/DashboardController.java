f9eb1d081fb6132e46a2944f30fe36ae121c6831
Marcin Koziel <marcinzkoziel@gmail.com>
2021-06-13 02:24:50 -0400

    add: visualization (LineChart) using roll no. for cross ref.

--- src/smartdice/controllers/DashboardController.java
+++ src/smartdice/controllers/DashboardController.java
@@ -5,8 +5,18 @@
  */
 package smartdice.controllers;
 
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 
+import javafx.scene.layout.StackPane;
+import smartdice.model.PlayerProfile;
+import smartdice.model.PlayerStat;
+import smartdice.model.SmartDiceGame;
+
+import javax.sound.sampled.Line;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,6 +26,16 @@ import java.util.ResourceBundle;
  * @author Marcin Koziel
  */
 public class DashboardController extends ClassController<DashboardController> {
+
+    @FXML
+    private StackPane chartPane;
+    private LineChart<Number, Number> scatterChart;  // LineChart - Roll No.
+    private XYChart.Series series;  // Roll No.
+
+    PlayerProfile currentPlayerProfile;
+
+    // TODO: Encapsulation
+    private static DashboardController dashboardController;
 
     public DashboardController(){
         super();
@@ -30,7 +50,64 @@ public class DashboardController extends ClassController<DashboardController> {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        scatterChart = createChart();
+        chartPane.getChildren().add(scatterChart);
+        dashboardController = this; // TODO: Encapsulation
+    }
+
+    // TODO: Encapsulation
+    public static DashboardController getInstance() {return dashboardController;}
+
+    private LineChart<Number, Number> createChart() {
+
+        currentPlayerProfile = SmartDiceGame.getInstance().getCurrentPlayerProfile();
+
+        final NumberAxis xAxis = new NumberAxis(0, 100, 10);
+        xAxis.setForceZeroInRange(false);
+        xAxis.autoRangingProperty().set(true);
+        final NumberAxis yAxis = new NumberAxis(0, 100,10);
+        final LineChart<Number,Number> sc = new LineChart<>(xAxis,yAxis);
+
+        // setup chart
+        sc.setTitle("Line Chart");
+        xAxis.setLabel("Dice Rounds");
+        xAxis.setAnimated(false);
+        yAxis.setLabel("Dice Range");
+        yAxis.setAutoRanging(false);
+
+        // add starting data
+        series = new ScatterChart.Series<Number,Number>();
+        series.setName("Roll Number");
+
+        for (int i = 0; i < currentPlayerProfile.getPlayerStat().getDiceGameList().size(); i++) {
+            series.getData().add(
+                    new ScatterChart.Data<Number, Number>(
+                            i+1,
+                            currentPlayerProfile.getPlayerStat().getDiceGameList().get(i).getDiceRoll()
+                    )
+            );
+        }
+
+        sc.getData().add(series);
+        return sc;
+    }
+
+    public void updateScatterChart() {
+
+        currentPlayerProfile = SmartDiceGame.getInstance().getCurrentPlayerProfile();
+
+        scatterChart.getData().get(0).getData().add(
+                new ScatterChart.Data<>(
+                        currentPlayerProfile.getPlayerStat().getDiceGameList().size(),
+                        currentPlayerProfile.getPlayerStat().getLastDiceGameRoundRoll()
+                )
+        );
+
+    }
+
+
+
+
+
     
 }
